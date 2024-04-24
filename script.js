b1d878cc44f95d91256d2c18d82317433c1898f2
Lucas Ngoves <137709296+Ngoveslucas@users.noreply.github.com>
2024-04-24 12:38:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-//Funcao para limpar o display
-function clearDisplay(){
-    document.getElementById('display').value = '';
-}
+document.getElementById("addTaskBtn").addEventListener("click", function(){
+    addTask();
+});
 
-//funcao para add numeros e operadores ao display
-function appendToDisplay(value){
-    document.getElementById('display').value += value;
-}
-
-//funcao para calcular o resultadao da expressao no display
-function calculate(){
-    try {
-        //obter o valor actual do display
-        var expression = document.getElementById('display').value;
-
-        //calcular o resultado da expressao
-        var result = eval(expression);
-
-        console.log('Resultado calculado:', result); //Adiciona mensagem de debug
-
-        //exibir o resultado no display
-        document.getElementById('display').value = result;
-        console.log('Resultado:', document.getElementById('display').value);
-    } catch (error) {
-        //se houver um erro na expressao, exibir "Erro" no display
-        document.getElementById('display').value = 'Erro';
+function addTask() {
+    var taskInput = document.getElementById("task");
+    var task = taskInput.value;
+    if (task.trim() !== "") {
+        var taskList = document.getElementById("tasks");
+        var li = document.createElement("li");
+        li.textContent = task;
+        taskList.appendChild(li);
+        taskInput.value = "";
     }
-} 
+}
